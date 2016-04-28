8e4924e292842b3ddcdd87ceca819c36d0327592
Kitson Kelly <me@kitsonkelly.com>
2016-04-28 08:41:15 +0100

    Refresh dependencies

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,11 +1,11 @@
-/// <reference path="../node_modules/@reactivex/rxjs/typings/main.d.ts" />
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
-/// <reference path="../node_modules/dojo-actions/typings/dojo-actions/dojo-actions-2.0.0-pre.d.ts" />
-/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-pre.d.ts" />
+/// <reference path="../node_modules/dojo-actions/typings/dojo-actions/dojo-actions.d.ts" />
+/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core.d.ts" />
 /// <reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts" />
-/// <reference path="../node_modules/dojo-compose/typings/dojo-compose/dojo-compose-2.0.0-pre.d.ts" />
-/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-beta.2.d.ts" />
-/// <reference path="../node_modules/dojo-widgets/typings/dojo-widgets/dojo-widgets-2.0.0-pre.d.ts" />
+/// <reference path="../node_modules/dojo-compose/typings/dojo-compose/dojo-compose.d.ts" />
+/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader.d.ts" />
+/// <reference path="../node_modules/dojo-widgets/typings/dojo-widgets/dojo-widgets.d.ts" />
+/// <reference path="../node_modules/@reactivex/rxjs/typings/main/ambient/es6-shim/index.d.ts" />
 
 declare module 'immutable/immutable' {
 	export = Immutable;
@@ -17,4 +17,15 @@ declare module 'maquette/maquette' {
 
 declare module 'rxjs/Rx' {
 	export * from 'node_modules/@reactivex/rxjs/dist/cjs/Rx';
+}
+
+/* For some reasons reactivex/rxjs is missing these */
+interface Iterator<T> {
+	next(value?: any): IteratorResult<T>;
+	return?(value?: any): IteratorResult<T>;
+	throw?(e?: any): IteratorResult<T>;
+}
+
+interface Iterable<T> {
+	[Symbol.iterator](): Iterator<T>;
 }
