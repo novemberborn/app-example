b71c1472fa88458acb7dce766886ca701f1b0139
Mark Wubben <mark@novemberborn.net>
2016-05-26 10:28:16 +0100

    new dojo-actions distribution format

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 	baseUrl: '../..',
 	packages: [
 		{ name: 'src', location: '_build/src' },
-		{ name: 'dojo-actions', location: 'node_modules/dojo-actions/dist' },
+		{ name: 'dojo-actions', location: 'node_modules/dojo-actions/dist/umd' },
 		{ name: 'dojo-app', location: 'node_modules/dojo-app/dist/umd' },
 		{ name: 'dojo-compose', location: 'node_modules/dojo-compose/dist/umd' },
 		{ name: 'dojo-core', location: 'node_modules/dojo-core' },
